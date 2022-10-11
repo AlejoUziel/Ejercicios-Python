7b39a14baa81f634b738efe357831d78e53cc714
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-10-11 14:33:30 -0600

    Variables

---
+++ variables/variables/variables.py
@@ -0,0 +1,11 @@
+
+variableInt=2 #variable tipo integer.
+variableFloat=10,30 #variable tipo float.
+variableString='hola, Alejandro'  #variable tipo string.
+variableString2="hola bro's" """variable tipo string con comillas dobles, Acepta los apostrofes 
+a diferencia de las comillas dobles."""
+print(variableInt)
+print(variableFloat)
+print(variableString)
+print(variableString2)
+
