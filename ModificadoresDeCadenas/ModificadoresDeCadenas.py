f7b921d6cacea3a075fa2709ca71c33897fe85ca
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-10-28 14:30:15 -0600

    Add files via upload

---
+++ ModificadoresDeCadenas/ModificadoresDeCadenas.py
@@ -0,0 +1,12 @@
+# strip() eliminara los espacios en una cadena.
+a=str("   hola Alejandro  ")
+print(a.strip()) # eliminira los espacios que estan delante y detras del texto.
+
+# replace() reemplaza una cadena con otra cadena.
+b=str("komo estas?")
+print(b.replace("k","c")) # reemplaza la letra k por la c.
+
+#split() divide las cadenas a subcadenas 
+c = "Ve a, dormir!"
+print(c.split(",")) # dividira la cadena en dos cadenas.
+
