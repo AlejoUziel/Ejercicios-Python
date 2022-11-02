dfed4d692b331e370e523ad5857365a1ef79761e
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-11-02 15:16:44 -0600

    Add files via upload

---
+++ HorasTrabajadas.py
@@ -0,0 +1,5 @@
+print("---CALCULADORA DE HORAS TRABAJADAS")
+horas=int(input("cuantas horas ha trabajado?"))
+pago= int(input("cuanto le pagan por hora?"))
+sueldo= horas*pago
+print("su sueldo es de: "+str(sueldo))
